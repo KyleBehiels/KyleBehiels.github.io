bd5fb2663725ece705ceab921d73563ef91e688f
Kyle Behiels <kylejamesbehiels@gmail.com>
2020-07-04 21:10:56 -0600

    Current project progress commit

---
+++ build/precache-manifest.c6344ea748ab5af5ad4caf2a726376f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ae5357dd9d7402bb7d0f30f7aab6cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77e480e3edf2478032fe",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c87ef9e0147ae004d573",
+    "url": "/static/css/main.99125cba.chunk.css"
+  },
+  {
+    "revision": "77e480e3edf2478032fe",
+    "url": "/static/js/2.a6833615.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.a6833615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87ef9e0147ae004d573",
+    "url": "/static/js/main.af2de296.chunk.js"
+  },
+  {
+    "revision": "185db819269ea21707f1",
+    "url": "/static/js/runtime-main.75a7c8a9.js"
+  },
+  {
+    "revision": "bbad38520f4e7fd554b84f282cacd37d",
+    "url": "/static/media/josefinsans-variablefont_wght-webfont.bbad3852.woff"
+  },
+  {
+    "revision": "a57d028b317eb0c04514b37773f9bd65",
+    "url": "/static/media/questrial-regular-webfont.a57d028b.woff2"
+  }
+]);
